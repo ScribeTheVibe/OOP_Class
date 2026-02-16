1e7ac1c1d6a20e852f70b90bc519afcc8da08a4a
ShuraTheSaint <dainiusknie@gmail.com>
2026-02-16 20:55:10 +0200

    refactor: clean up simple array script

--- code/Rezultatai.cpp
+++ code/Rezultatai.cpp
@@ -1,54 +1,80 @@
 #include <iostream>
-#include <vector>
-#include <fstream>
+#include <algorithm>
 
 using std::string;
 using std::cout;
-using std::ofstream;
 using std::cin;
 using std::endl;
-
+using std::sort;
 
 struct studentas {
-    string vardas = "D", pavarde = "K";
-    int *pazimys;
+    string vardas, pavarde;
+    int* pazimys;
     int rezultatas = 0;
+    double vidurkis = 0;
+    double mediana = 0;
 };
 
 int main()
 {
-    studentas *s = new studentas[3];
-    
-    for (int i=0; i<3; i++ )
+    int n, p;
+
+    cout << "Iveskite studentu kieki" << endl;
+    cin >> n;
+
+    cout << "Iveskite pazymiu kieki" << endl;
+    cin >> p;
+
+    studentas* s = new studentas[n];
+
+    for(int i = 0; i < n; i++)
     {
-        s[i].pazimys = new int[2];
-    }
-    
-    for(int i=0; i<2; i++)
-    {
-        for (int j=0; j<3; j++ )
+        cout << "Iveskite " << i+1 << " studento varda" << endl;
+        cin >> s[i].vardas;
+
+        cout << "Iveskite " << i+1 << " studento pavarde" << endl;
+        cin >> s[i].pavarde;
+
+        s[i].pazimys = new int[p];
+
+        for(int j = 0; j < p; j++)
         {
-            cin>>s[j].pazimys[i];
+            cout << "Iveskite pazymi" << endl;
+            cin >> s[i].pazimys[j];
+
+            s[i].vidurkis += s[i].pazimys[j];
         }
-    }
-    
-    for(int i=0; i<2; i++)
-    {
-        for (int j=0; j<3; j++ )
+
+        cout << "Iveskite egzamino rezultata" << endl;
+        cin >> s[i].rezultatas;
+
+        sort(s[i].pazimys, s[i].pazimys + p);
+        if(p % 2 == 0)
         {
-            s[j].rezultatas += s[j].pazimys[i];
+            s[i].mediana =
+                (s[i].pazimys[p/2 - 1] + s[i].pazimys[p/2]) / 2.0;
         }
+        else
+        {
+            s[i].mediana = s[i].pazimys[p/2];
+        }
+        s[i].vidurkis /= p;
     }
 
-    ofstream O ("Rezultatai.txt");
-    for(int i=0; i<3; i++)
+    for(int i = 0; i < n; i++)
     {
-        O<<s[i].rezultatas/2.0*0.4+10*0.6<<endl;
+        cout<<s[i].vardas<<" "<<s[i].pavarde<<" galutinis rezultatas:"<<endl;
+        cout<< s[i].vidurkis * 0.4 + s[i].rezultatas * 0.6<< endl;
+        cout<<s[i].vardas<<" "<<s[i].pavarde<<" mediana:"<<endl;
+        cout<<s[i].mediana<<endl;
     }
-    
-    for(int i=0; i<3; i++)
+
+    for(int i = 0; i < n; i++)
     {
-        delete[](s[i].pazimys);
+        delete[] s[i].pazimys;
     }
+
+    delete[] s;
+
     return 0;
 }
--- code/RezultataiVector.cpp
+++ code/RezultataiVector.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <vector>
-#include <fstream>
+#include <algorithm>
 
 using std::string;
 using std::vector;
-using std::ofstream;
+using std::cout;
 using std::cin;
 using std::endl;
+using std::sort;
 
 
 struct studentas {
-    string vardas = "D", pavarde = "K";
+    string vardas, pavarde;
     vector<int> pazimys;
     int rezultatas = 0;
     double vidurkis = 0;
+    double mediana = 0;
 };
 
 int main()
@@ -21,30 +23,49 @@ int main()
     vector<studentas> s;
     studentas tempS;
     int n, p;
-    cin>>n>>p;
+    cout<<"Iveskite studentu kieki"<<endl;
+    cin>>n;
+    cout<<"Iveskite pazymiu kieki"<<endl;
+    cin>>p;
     s.reserve(n);
 
     int temp;
     for(int i=0; i<n; i++)
     {
+        cout<<"Iveskite "<<i+1<<" studento varda"<<endl;
+        cin>>tempS.vardas;
+        cout<<"Iveskite "<<i+1<<" studento pavarde"<<endl;
+        cin>>tempS.pavarde;
         for(int j=0; j<p; j++)
         {
+            cout<<"Iveskite pazymi"<<endl;
             cin>>temp;
             tempS.pazimys.push_back(temp);
             tempS.vidurkis += temp;
+        }
+        cout<<"Iveskite egzamino rezultata"<<endl;
+        cin>>tempS.rezultatas;
+        sort(tempS.pazimys.begin(), tempS.pazimys.end());
+        if(p % 2 == 0)
+        {
+            tempS.mediana = (tempS.pazimys[p/2 - 1] + tempS.pazimys[p/2]) / 2.0;
+        }
+        else
+        {
+            tempS.mediana = tempS.pazimys[p/2];
         }
         tempS.vidurkis /= p;
         s.push_back(tempS);
         tempS.pazimys.clear();
         tempS.vidurkis = 0;
     }
-
-    std::cout<<"test";
-
-    ofstream O ("Rezultatai.txt");
+    
     for(int i=0; i<s.size(); i++)
     {
-        O<<s[i].vidurkis*0.4+s[i].rezultatas*0.6<<endl;
+        cout<<s[i].vardas<<" "<<s[i].pavarde<<" galutinis rezultatas:"<<endl;
+        cout<<s[i].vidurkis*0.4+s[i].rezultatas*0.6<<endl;
+        cout<<s[i].vardas<<" "<<s[i].pavarde<<" mediana:"<<endl;
+        cout<<s[i].mediana<<endl;
     }
     return 0;
 }
