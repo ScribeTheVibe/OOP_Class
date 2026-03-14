1b33809631553831ad814eae036e510fee8ccc32
ShuraTheSaint <dainiusknie@gmail.com>
2026-03-14 21:29:55 +0200

    feat: expand header file for v0.4

--- code/Headers.h
+++ code/Headers.h
@@ -10,6 +10,7 @@
 #include <iomanip>
 #include <sstream>
 #include <string>
+#include <chrono>
 
 using std::cin;
 using std::cout;
@@ -22,6 +23,8 @@ using std::setw;
 using std::sort;
 using std::string;
 using std::vector;
+using std::chrono::duration;
+using std::chrono::high_resolution_clock;
 
 struct studentas
 {
@@ -37,6 +40,17 @@ struct studentas
 double skaiciuotiMediana(vector<int> &v);
 int saugusInt(string tekstas, int min, int max);
 void sortS(vector<studentas> &s, int sortType);
+
+// v0.4
+void generuotiFaila(const string &filename, int studentCount, int gradeCount);
+void generuotiVisusFailus();
+bool nuskaitytiIsFailo(const string &filename, vector<studentas> &s);
+void isskirtiStudentus(const vector<studentas> &visi,
+                       vector<studentas> &gerai,
+                       vector<studentas> &blogai);
+void issaugotiStudentus(const vector<studentas> &s, const string &filename);
+void testas1_failuKurimas();
+void testas2_duomenuApdorojimas(const string &filename);
 
 extern string vardai[15];
 extern string pavardes[15];
